15f74f006a42b89f9cc8b2720235876ff4cd7691
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-01-16 14:12:37 -0300

    refactor: initial app refactors

---
+++ lib/task_list_app.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:lista_tarefas_app/pages/main/main_page.dart';
+
+class TaskListApp extends StatelessWidget {
+  const TaskListApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.system,
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      darkTheme: ThemeData.dark(
+        useMaterial3: true,
+      ),
+      home: const MainPage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
