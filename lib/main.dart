909bb90a62566052fe95a6e1c4c2b7915fa7ce87
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-01-17 00:57:26 -0300

    refactor: remove database get it

--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,12 @@ import 'package:lista_tarefas_app/repositories/impl/sqlite_database.dart';
 import 'package:lista_tarefas_app/repositories/impl/task_sqlite_repository.dart';
 import 'package:lista_tarefas_app/repositories/task_repository.dart';
 import 'package:lista_tarefas_app/task_list_app.dart';
-import 'package:sqflite/sqflite.dart';
 
 var getIt = GetIt.asNewInstance();
 
 void setupGetIt() {
-  getIt.registerSingletonAsync<Database>(
-    () async => await SQLiteDatabase.getDatabase(),
-  );
-  getIt.registerSingletonWithDependencies<TaskRepository>(
-    () => TaskSQLiteRepository(getIt<Database>()),
-    dependsOn: [Database],
+  getIt.registerSingletonAsync<TaskRepository>(
+    () async => TaskSQLiteRepository(await SQLiteDatabase.getDatabase()),
   );
 }
 
