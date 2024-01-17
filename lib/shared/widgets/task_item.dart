32be23392dc2d6d8aa1a9211d821aad92e2f4a0d
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-01-17 01:04:06 -0300

    feat: use task provider in task item

--- lib/shared/widgets/task_item.dart
+++ lib/shared/widgets/task_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:lista_tarefas_app/models/task.dart';
+import 'package:lista_tarefas_app/services/task_provider.dart';
 
 class TaskItem extends StatelessWidget {
   final Task task;
@@ -10,12 +12,22 @@ class TaskItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Dismissible(
       key: Key(task.id.toString()),
-      onDismissed: (_) {},
-      child: ListTile(
-        title: Text(task.description),
-        trailing: Switch(
-          value: task.completed,
-          onChanged: (value) {},
+      onDismissed: (_) async {
+        await Provider.of<TaskProvider>(context, listen: false).remove(task.id);
+      },
+      child: Card(
+        elevation: 2,
+        child: ListTile(
+          leading: const Icon(Icons.task_outlined),
+          title: Text(task.description),
+          trailing: Switch(
+            value: task.completed,
+            onChanged: (value) async {
+              task.completed = value;
+              await Provider.of<TaskProvider>(context, listen: false)
+                  .update(task.id, task);
+            },
+          ),
         ),
       ),
     );
