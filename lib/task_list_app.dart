9d8e8e22544b10086153a3eaf11f047a023e68bf
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-01-17 01:02:03 -0300

    fix: show splash screen to wait async get it inicialize

--- lib/task_list_app.dart
+++ lib/task_list_app.dart
@@ -1,33 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:lista_tarefas_app/main.dart';
-import 'package:lista_tarefas_app/pages/main/main_page.dart';
-import 'package:lista_tarefas_app/repositories/task_repository.dart';
-import 'package:lista_tarefas_app/services/task_provider.dart';
+import 'package:lista_tarefas_app/pages/splash_screen/splash_screen_delayed_page.dart';
 
 class TaskListApp extends StatelessWidget {
   const TaskListApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (_) => TaskProvider(getIt<TaskRepository>()),
-        ),
-      ],
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        debugShowCheckedModeBanner: false,
-        themeMode: ThemeMode.system,
-        theme: ThemeData(
-          useMaterial3: true,
-        ),
-        darkTheme: ThemeData.dark(
-          useMaterial3: true,
-        ),
-        home: const MainPage(title: 'Lista de Tarefas'),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.system,
+      theme: ThemeData(
+        useMaterial3: true,
       ),
+      darkTheme: ThemeData.dark(
+        useMaterial3: true,
+      ),
+      home: const SplashScreenDelayedPage(),
     );
   }
 }
